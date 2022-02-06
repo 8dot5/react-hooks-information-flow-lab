ff81b301de77e2d74de717990b259a3a0024b609
Chun Yi <8.5by11@gmail.com>
2022-02-06 17:50:02 -0500

    commit tree b40ea556ba0e2ee211fefd95e4a3c96601d66e4c

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+function Header({ onDarkModeClick, mode }) {
+    return (
+        <header>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>
+            {mode ? "Light" : "Dark"} Mode
+            </button>
+        </header>
+    )
+}
+
+export default Header;
